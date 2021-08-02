c4b64be61180f66aadb6b8adc6f70b430e5fcf1f
JaredG <65161768+JaredG59@users.noreply.github.com>
2021-08-01 18:12:25 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 
 
-const CONNECTION_URL = 'mongodb+srv://cs160astro:e2lPXqy8h1wjzCTt@cluster0.2npw1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = '';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
